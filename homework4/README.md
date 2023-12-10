6ac7bfd6e8fe31caa2758e33b3842af82ef81c63
Hendra Priyana M <hendrapm99@gmail.com>
2023-12-11 06:06:49 +0700

    [IMP] adding homework4

---
+++ homework4/README.md
@@ -0,0 +1,3 @@
+## Homework 3
+
+create calculator function ``` input value a (operator) input value b = result```
---
+++ homework4/calculator/src/main/java/com/javatraining/Runner.java
@@ -0,0 +1,39 @@
+package com.javatraining;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        // instantiate class
+        Operation operation = new Operation();
+        int valueX, valueY, operator;
+
+        Scanner inputUser = new Scanner(System.in);
+
+        System.out.print("Input Nilai X     : ");
+        valueX = inputUser.nextInt();
+        System.out.print("Input Nilai Y     : ");
+        valueY = inputUser.nextInt();
+
+        System.out.print("Operator:\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\nPilih Operator    : ");
+        operator = inputUser.nextInt();
+        switch (operator) {
+            case 1:
+                operation.add(valueX, valueY);
+                break;
+            case 2:
+                operation.sub(valueX, valueY);
+                break;
+            case 3:
+                operation.multiply(valueX, valueY);
+                break;
+            case 4:
+                operation.div(valueX, valueY);
+                break;
+        
+            default:
+                System.out.println("Operator tidak tersedia");
+        }
+        inputUser.close();
+    }
+}
